04b95dcc20ba296c5655b6c82648408681c10097
Jundong Qiao <qiaojundong@gmail.com>
2017-12-11 16:34:57 -0500

    extract frames from video clips

---
+++ frameExtractor.py
@@ -0,0 +1,135 @@
+import cv2
+import os
+import sys
+import time
+import argparse
+
+def frameExtractor(videofile, sample_frequency):
+	# Read video file
+	vidcap = cv2.VideoCapture(videofile)
+
+	"""
+	Obtaining the video info
+	"""
+	if vidcap.isOpened():
+		print "The imported file is readable."
+		
+	else:
+		print "could not open the input file"
+
+		print "converting MP4 file..."
+		os.system('ffmpeg -i %s -c:v copy %s_Converted.mp4' %(videofile, videofile[:-4]))
+		vidcap = cv2.VideoCapture('%s_Converted.MP4' % videofile[:-4])	
+
+	length = int(vidcap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
+	width  = int(vidcap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
+	height = int(vidcap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
+	fps    = vidcap.get(cv2.cv.CV_CAP_PROP_FPS)
+
+	print 'The fps of the file is: ', fps
+
+	"""
+	Reading the video and saving it into images
+	"""
+	data_dir = '%s' %videofile[:-4]
+	data_dir = data_dir + '/sampledFrames'
+	if not os.path.exists(data_dir):
+	  os.makedirs(data_dir)
+
+	count = 0
+	while True:
+
+		print count
+
+		grabbed,image = vidcap.read()
+
+		if not grabbed:
+
+			print 'Failed to grab the frame'
+			break
+
+		print 'Read a new frame: grabbed!'
+		count += 1
+		if count%sample_frequency == 0:
+			cv2.imwrite(os.path.join(data_dir, "%05d.png" % count), image)
+
+
+def check_run(videofile, sample_frequency):
+	
+	vidcap = cv2.VideoCapture(videofile)
+	if vidcap.isOpened():
+		print "The imported file is readable."
+
+		t0 = time.time()
+		frameExtractor(videofile, sample_frequency)
+		t1 = time.time()
+
+		t = t1 - t0
+		print "The process took %.2f mins." %(round(t/60., 2))
+
+	else:
+		print "Could not open the input file"
+
+		print "Converting MP4 file..."
+		os.system('ffmpeg -i %s -c:v copy %s_Converted.mp4' %(videofile, videofile[:-4]))
+		vidcap = cv2.VideoCapture('%s_Converted.MP4' % videofile[:-4])	
+
+		print 'Conversion Completed!'
+		videofile_converted = '%s_Converted.MP4' % videofile[:-4]
+		t0 = time.time()
+		frameExtractor(videofile_converted, sample_frequency)
+		t1 = time.time()
+
+		t = t1 - t0
+		print "The process took %.2f mins." %(round(t/60., 2))
+
+
+#####################
+###### Running ######
+#####################
+# '''
+# if __name__ == "__main__":
+# 	print '~~~ Loading .MP4 file...'
+# 	videofile = sys.argv[1]
+# 	sample_frequency = int(sys.argv[2])
+# 	
+# 	vidcap = cv2.VideoCapture(videofile)
+# 	
+# 	if vidcap.isOpened():
+# 		print "The imported file is readable."
+# 
+# 		t0 = time.time()
+# 		frameExtractor(videofile, sample_frequency)
+# 		t1 = time.time()
+# 
+# 		t = t1 - t0
+# 		print "The process took %.2f mins." %(round(t/60., 2))
+# 
+# 	else:
+# 		print "Could not open the input file"
+# 
+# 		print "Converting MP4 file..."
+# 		os.system('ffmpeg -i %s -c:v copy %s_Converted.mp4' %(videofile, videofile[:-4]))
+# 		vidcap = cv2.VideoCapture('%s_Converted.MP4' % videofile[:-4])	
+# 
+# 		print 'Conversion Completed!'
+# 		videofile_converted = '%s_Converted.MP4' % videofile[:-4]
+# 		t0 = time.time()
+# 		frameExtractor(videofile_converted, sample_frequency)
+# 		t1 = time.time()
+# 
+# 		t = t1 - t0
+# 		print "The process took %.2f mins." %(round(t/60., 2))
+# '''
+
+if __name__ == "__main__":
+	print '~~~ Loading .MP4 file... & Sample Frequency ~~~'
+	ap = argparse.ArgumentParser()
+	ap.add_argument('-v', '--video', required=True, help='filename of one video clip')
+	ap.add_argument('-n', '--frequency', type=int, required=True, help='sample frequency of the video frames')
+	args = vars(ap.parse_args())
+
+	videofile = args['video']
+	sample_frequency = args['frequency']
+	
+	check_run(videofile, sample_frequency)
