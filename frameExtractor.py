0b63a63b2e38fc2c4d309c8217507d1eba24a2c9
Jundong Qiao <qiaojundong@gmail.com>
2017-12-11 17:02:28 -0500

    Update frameExtractor.py

--- frameExtractor.py
+++ frameExtractor.py
@@ -87,41 +87,6 @@ def check_run(videofile, sample_frequency):
 #####################
 ###### Running ######
 #####################
-# '''
-# if __name__ == "__main__":
-# 	print '~~~ Loading .MP4 file...'
-# 	videofile = sys.argv[1]
-# 	sample_frequency = int(sys.argv[2])
-# 	
-# 	vidcap = cv2.VideoCapture(videofile)
-# 	
-# 	if vidcap.isOpened():
-# 		print "The imported file is readable."
-# 
-# 		t0 = time.time()
-# 		frameExtractor(videofile, sample_frequency)
-# 		t1 = time.time()
-# 
-# 		t = t1 - t0
-# 		print "The process took %.2f mins." %(round(t/60., 2))
-# 
-# 	else:
-# 		print "Could not open the input file"
-# 
-# 		print "Converting MP4 file..."
-# 		os.system('ffmpeg -i %s -c:v copy %s_Converted.mp4' %(videofile, videofile[:-4]))
-# 		vidcap = cv2.VideoCapture('%s_Converted.MP4' % videofile[:-4])	
-# 
-# 		print 'Conversion Completed!'
-# 		videofile_converted = '%s_Converted.MP4' % videofile[:-4]
-# 		t0 = time.time()
-# 		frameExtractor(videofile_converted, sample_frequency)
-# 		t1 = time.time()
-# 
-# 		t = t1 - t0
-# 		print "The process took %.2f mins." %(round(t/60., 2))
-# '''
-
 if __name__ == "__main__":
 	print '~~~ Loading .MP4 file... & Sample Frequency ~~~'
 	ap = argparse.ArgumentParser()
